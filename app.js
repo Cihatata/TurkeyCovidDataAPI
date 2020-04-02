5d82b4a35d29afedd4ce3d188c6699f6f6665056
cihatata <omercihatataa@gmail.com>
2020-04-02 22:50:59 +0300

    new req /provinces

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 const cors = require("cors")
 const service = require('./index')
+const province = require('./provinces')
 
 app.use(cors())
 
 app.get('/', async (req, res) => {
    const result = await service.getResult
     res.send(result);
+})
+app.get('/provinces', (req, res) => {
+    let result = province.provinces
+    res.send(result)
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
